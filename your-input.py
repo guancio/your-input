2a5fddee8d10075ce559fa0c7b9de082001c21e6
Roberto Guanciale <guancio@gmail.com>
2016-04-20 20:29:12 +0000

    you speed, acceleration

--- your-input.py
+++ your-input.py
@@ -46,6 +46,9 @@ events = (
 left = False
 right = False
 
+xspeed = 0
+yspeed = 0
+
 with uinput.Device(events) as device:
     while loopQuit == False:
 
@@ -64,13 +67,7 @@ with uinput.Device(events) as device:
         # print(outstr)
 
         for event in pygame.event.get():
-            if event.type == QUIT:
-                loopQuit = True
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    loopQuit = True
-
-        # other event tests, but polling seems to work better in main loop
+            continue
         # if event.type == pygame.JOYBUTTONDOWN:
         #print("joy button down")
         # if event.type == pygame.JOYBUTTONUP:
@@ -83,26 +80,49 @@ with uinput.Device(events) as device:
         # print("joy axis motion")
     
         # pygame.display.update()
-        if (joystick.get_button(4)):
-            device.emit(uinput.REL_Y, -5)
-        if (joystick.get_button(5)):
-            device.emit(uinput.REL_X, 5)
-        if (joystick.get_button(6)):
-            device.emit(uinput.REL_Y, 5)
-        if (joystick.get_button(7)):
-            device.emit(uinput.REL_X, -5)
-        device.emit(uinput.REL_X, int(joystick.get_axis(0)*15))
-        device.emit(uinput.REL_Y, int(joystick.get_axis(1)*15))
-        if (joystick.get_button(12) and (not left)):
+        # if (joystick.get_button(4)):
+        #     device.emit(uinput.REL_Y, -5)
+        # if (joystick.get_button(5)):
+        #     device.emit(uinput.REL_X, 5)
+        # if (joystick.get_button(6)):
+        #     device.emit(uinput.REL_Y, 5)
+        # if (joystick.get_button(7)):
+        #     device.emit(uinput.REL_X, -5)
+        if (abs(joystick.get_axis(0)) < 0.1 and abs(joystick.get_axis(2)) < 0.1):
+            xspeed = 0
+        if (abs(joystick.get_axis(0)) >= 0.1):
+            xspeed += 3*joystick.get_axis(0)
+        if (abs(joystick.get_axis(2)) >= 0.1):
+            xspeed = 10*joystick.get_axis(2)
+        if (xspeed > 60):
+            xspeed = 60    
+            
+        if (abs(joystick.get_axis(1)) < 0.1 and abs(joystick.get_axis(3)) < 0.1):
+            yspeed = 0
+        if (abs(joystick.get_axis(1)) >= 0.1):
+            yspeed += 3*joystick.get_axis(1)
+        if (abs(joystick.get_axis(3)) >= 0.1):
+            yspeed = 10*joystick.get_axis(3)
+        if (yspeed > 60):
+            yspeed = 60    
+
+            
+        if (xspeed > 0 and yspeed > 0):
+            xspeed = max(xspeed, yspeed)
+            yspeed = xspeed
+            
+        device.emit(uinput.REL_X, int(xspeed))
+        device.emit(uinput.REL_Y, int(yspeed))
+        if ((joystick.get_button(12) or joystick.get_button(10)) and (not left)):
             device.emit(uinput.BTN_LEFT, 1);
             left = True
-        if (not joystick.get_button(12) and left):
+        if (not joystick.get_button(12) and not joystick.get_button(10) and left):
             device.emit(uinput.BTN_LEFT, 0);
             left = False
-        if (joystick.get_button(15) and (not left)):
+        if ((joystick.get_button(15) or joystick.get_button(8))and (not right)):
             device.emit(uinput.BTN_RIGHT, 1);
             right = True
-        if (not joystick.get_button(15) and left):
+        if (not joystick.get_button(15) and not joystick.get_button(8) and right):
             device.emit(uinput.BTN_RIGHT, 0);
             right = False
         time.sleep(interval)
